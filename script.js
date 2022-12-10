6417a4df5b108c0e8c6e80ee48e5c24a36a4699d
I527245 <rajeev.kumar13@sap.com>
2022-12-10 15:09:21 +0530

    ip

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ async function display(){
     console.log("hello");
     const name1 = document.getElementById("name");
     // console.log(name);
-    const data  = await fetch("http://localhost:3000/details")
+    const data  = await fetch("http://3.94.107.178:3000/details")
     .then(response => response.json());
-    console.log(data)
+    console.log(data);
     name1.innerHTML = data;
    
     //    name.innerHtml = "ad";
