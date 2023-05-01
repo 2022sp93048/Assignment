72debd9f40753e4eb74b10ee86660b70a1f2cdf1
2022sp93048 <120042350+2022sp93048@users.noreply.github.com>
2023-05-01 23:13:16 +0530

    Update script.js
    script file

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 async function display(){
     console.log("hello");
     const name1 = document.getElementById("name");
-    // console.log(name);
     const data  = await fetch("http://3.94.107.178:3000/details")
     .then(response => response.json());
     console.log(data);
     name1.innerHTML = data;
-   
-    //    name.innerHtml = "ad";
 }
-
-// async function getData(){
-    
-//     return data;
-// }
